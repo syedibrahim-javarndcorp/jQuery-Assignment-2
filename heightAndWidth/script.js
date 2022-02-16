f97b088dad0466a3d95d23a738999d1da7eef196
syedibrahim-javarndcorp <syedibrahim.javarndcorp@gmail.com>
2022-02-16 10:01:46 -0500

    height and width

---
+++ heightAndWidth/script.js
@@ -0,0 +1,4 @@
+var width = $(".box").width();
+var height= $(".box").height();
+$('.height').html("<h2> The height is : " + height + "</h2>")
+$('.width').html("<h2> The width is : " + width + "</h2>")
